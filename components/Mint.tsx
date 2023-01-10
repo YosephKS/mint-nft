748e1c81e9f1629c4fa0a0f7d4d65b6b4e95f0c5
Yoseph Kurnia Soenggoro <yosephkurniasoenggoro@gmail.com>
2023-01-10 21:08:23 +0700

    minor fix on Image component

--- components/Mint.tsx
+++ components/Mint.tsx
@@ -1,17 +1,21 @@
-import { useState, useEffect } from 'react';
-import { ConnectWallet } from './ConnectWallet';
-import { SwitchNetwork } from './SwitchNetwork';
-import { UploadNft } from './UploadNft';
-import { Button, LoadingDots, Text } from '@vercel/examples-ui';
-import { useAccount, useNetwork } from 'wagmi';
-import { useSession } from "next-auth/react"
-import { NETWORK_ID } from '../helpers/constant.helpers';
-import { useRouter } from 'next/router'
-import { useContractRead, useContractWrite, usePrepareContractWrite } from 'wagmi';
-import axios from 'axios';
-import Image from 'next/image';
+import { useState, useEffect } from "react";
+import { ConnectWallet } from "./ConnectWallet";
+import { SwitchNetwork } from "./SwitchNetwork";
+import { UploadNft } from "./UploadNft";
+import { Button, LoadingDots, Text } from "@vercel/examples-ui";
+import { useAccount, useNetwork } from "wagmi";
+import { useSession } from "next-auth/react";
+import { NETWORK_ID } from "../helpers/constant.helpers";
+import { useRouter } from "next/router";
+import {
+  useContractRead,
+  useContractWrite,
+  usePrepareContractWrite,
+} from "wagmi";
+import axios from "axios";
+import Image from "next/image";
 import NFTAbi from "../hardhat/artifacts/contracts/NFT.sol/NFT.json";
-import NFTAddress from '../utils/constant';
+import NFTAddress from "../utils/constant";
 
 enum MintState {
   Connect,
@@ -34,13 +38,13 @@ export const Mint: React.VFC = () => {
   const { data, refetch } = useContractRead({
     address: NFTAddress,
     abi: NFTAbi.abi,
-    functionName: 'totalSupply',
-  })
+    functionName: "totalSupply",
+  });
 
   const { config } = usePrepareContractWrite({
     address: NFTAddress,
     abi: NFTAbi.abi,
-    functionName: 'safeMint',
+    functionName: "safeMint",
     args: [address, tokenUri],
     onSuccess: () => {
       try {
@@ -50,66 +54,69 @@ export const Mint: React.VFC = () => {
             `https://testnets.opensea.io/assets/goerli/${NFTAddress}/${tokenId}`
           );
         }, 10000);
-      }
-      catch (e) {
+      } catch (e) {
         console.error(e);
       }
-    }
-  })
+    },
+  });
   const { writeAsync, isSuccess } = useContractWrite(config);
 
   useEffect(() => {
     if (!address || !isConnected || status !== "authenticated") {
-      setState(MintState.Connect)
+      setState(MintState.Connect);
     } else if (chain?.id !== NETWORK_ID) {
-      setState(MintState.ConfirmNetwork)
+      setState(MintState.ConfirmNetwork);
     } else {
-      setState(MintState.Upload)
+      setState(MintState.Upload);
     }
-  }, [address, chain?.id, isConnected, status])
+  }, [address, chain?.id, isConnected, status]);
 
   const handleMint = async () => {
     try {
-      setLoading(true)
+      setLoading(true);
 
-      const { data: dataRes } = await axios.post('/api/ipfs/upload-folder', {
-        uploadArray: [
-          {
-            path: "metadata.json",
-            // @ts-ignore
-            content: {
-              name: 'My own NFT by Vercel',
-              description: 'NFTs minted using Vercel and Next.js',
-              //@ts-ignore
-              image: asset,
-            }
-          }
-        ]
-      }, {
-        headers: {
-          'Content-Type': 'application/json',
+      const { data: dataRes } = await axios.post(
+        "/api/ipfs/upload-folder",
+        {
+          uploadArray: [
+            {
+              path: "metadata.json",
+              // @ts-ignore
+              content: {
+                name: "My own NFT by Vercel",
+                description: "NFTs minted using Vercel and Next.js",
+                //@ts-ignore
+                image: asset,
+              },
+            },
+          ],
         },
-      });
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
       setTokenUri(dataRes?.[0]?.path);
       refetch();
     } catch (e) {
       console.error(e);
       setLoading(false);
     }
-  }
+  };
 
   const onUploadComplete = async (asset: string) => {
-    setAsset(asset)
-    setState(MintState.ConfirmMint)
-    setLoading(false)
-  }
+    setAsset(asset);
+    setState(MintState.ConfirmMint);
+    setLoading(false);
+  };
 
   useEffect(() => {
     if (tokenUri && !isSuccess) {
-      writeAsync?.()
+      writeAsync?.();
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [tokenUri, isSuccess])
+  }, [tokenUri, isSuccess]);
 
   return (
     <div className="inline-block align-bottom text-left overflow-hidden  transform transition-all sm:my-8 sm:align-middle  ">
@@ -126,21 +133,24 @@ export const Mint: React.VFC = () => {
             Your image will be minted as an ERC721 Token. It can happen that
             images stored on IPFS as it is a distributed file hosting system
             that can fail. This is still the prefered method of choice to host
-            in the NFT community as it is decentralized.{' '}
+            in the NFT community as it is decentralized.{" "}
             <span className="underline italic">
               This process might take up to 1 minute to complete. Please refresh
               the OpenSea page if you didn&apos;t see your NFT.
             </span>
           </Text>
           <section className="relative w-full pb-[20%] h-48 pb-6 mt-12">
-            <Image
-              className="rounded-xl"
-              src={asset ?? ""}
-              alt="The image that will be minted as an NFT"
-              fill
-            />
+            {asset ? (
+              <Image
+                className="rounded-xl"
+                src={asset}
+                alt="The image that will be minted as an NFT"
+                fill
+              />
+            ) : (
+              <></>
+            )}
           </section>
-
           <section className="flex justify-center mt-6">
             <Button
               size="lg"
@@ -148,11 +158,11 @@ export const Mint: React.VFC = () => {
               onClick={handleMint}
               disabled={!asset || isLoading}
             >
-              {isLoading ? <LoadingDots /> : 'Mint'}
+              {isLoading ? <LoadingDots /> : "Mint"}
             </Button>
           </section>
         </>
       )}
     </div>
-  )
-}
+  );
+};
